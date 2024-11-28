cbcd5db7fa4384dfe1ff353d907365a98bf802b4
Biblioteca Central Cesar Lattes <laboratorio@bc.unicamp.br>
2024-11-28 12:12:24 -0300

    Teste e comentação do código

--- config/database.js
+++ config/database.js
@@ -8,35 +8,39 @@ import dotenv from "dotenv";
 // qual dialect define o tipo de bd (SQLite) e storage o
 // caminho do arquivo onde os dados do banco serão
 // armazenados
-const sequelize = new Sequelize({ 
-    host: "localhost",
-    dialect: "sqlite",
-    //storage: "./banco.db",
-    storage: "./database.sqlite",
-    define: {
-        timestamps: false,
-    }
-});
-
-// dotenv.config();
-
-// const sequelize = new Sequelize(
-//     process.env.DB_PROD_NAME,
-//     process.env.DB_PROD_USER,
-//     '4xL5gzL8I8zvd4gF',
-//     {
-//         host: process.env.DB_PROD_HOST,
-//         port: process.env.DB_PROD_PORT,
-//         dialect: process.env.DB_PROD_DIALECT,
-//         dialectOptions: {
-//             ssl: {
-//                 require: true,
-//                 rejectUnauthorized: false,
-//             },
-//         },
-//         logging: false,
+// const sequelize = new Sequelize({ 
+//     host: "localhost",
+//     dialect: "sqlite",
+//     // storage: "./banco.db",
+//     storage: "./database.sqlite",
+//     define: {
+//         timestamps: false,
 //     }
-// );
+// });
+
+dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
+
+// Conexão com o banco de dados PROD (produção),
+// utilizando as variáveis de ambiente configuradas no .env.
+// Dialect define o tipo de banco de dados (MySQL, Postgres, SQLite, etc.),
+// e as opções adicionais como ssl são configuradas para maior segurança.
+const sequelize = new Sequelize(
+    process.env.DB_PROD_NAME, // Nome do banco de dados
+    process.env.DB_PROD_USER, // Usuário do banco
+    '4xL5gzL8I8zvd4gF',       // Senha do banco (pode ser extraída de variáveis de ambiente por questões de segurança)
+    {
+        host: process.env.DB_PROD_HOST, // Host do banco de dados
+        port: process.env.DB_PROD_PORT, // Porta do banco de dados
+        dialect: process.env.DB_PROD_DIALECT, // Tipo do banco de dados (Postgres, MySQL, etc.)
+        dialectOptions: {
+            ssl: { // Configuração SSL para conexões seguras
+                require: true,
+                rejectUnauthorized: false, // Permite certificados SSL autoassinados
+            },
+        },
+        logging: false, // Desabilita logs SQL no console para manter o terminal limpo
+    }
+);
 
 // Função assíncrona para verificar o teste de conexão com o banco de dados foi feito corretamente
 const testConnection = async () => {
--- controllers/publicacaoController.js
+++ controllers/publicacaoController.js
@@ -1,14 +1,14 @@
-import  Publicacao  from "../models/Publicacoes.js";
-import  Usuario  from "../models/Usuarios.js";
-import  Comentario  from "../models/Comentarios.js";
+import Publicacao from "../models/Publicacoes.js"; // Modelo de Publicação
+import Usuario from "../models/Usuarios.js"; // Modelo de Usuário
+import Comentario from "../models/Comentarios.js"; // Modelo de Comentário
 
+// Função para criar uma nova publicação
 export const createPublicacao = async (req, res) => {
-
     try {
-        const { usuario_id, publicacao } = req.body;
+        const { usuario_id, publicacao } = req.body; // Obtém os dados do corpo da requisição
 
-        const usuario = await Usuario.findByPk(usuario_id);
-    
+        const usuario = await Usuario.findByPk(usuario_id); // Busca o usuário pelo ID
+
         // Verifica se o usuário existe
         if (!usuario) {
             return res.status(400).json({ error: "Usuário não encontrado" });
@@ -23,59 +23,61 @@ export const createPublicacao = async (req, res) => {
         const novaPublicacao = await Publicacao.create({
             publicacao,
             usuario_id,
-            criado_em: new Date(),
+            criado_em: new Date(), // Define a data de criação como a data atual
         });
 
         // Retorna a publicação criada
         return res.status(201).json({
-            publicacao_id: novaPublicacao.id,
+            publicacao_id: novaPublicacao.id, // Retorna o ID da publicação criada
         });
-
     } catch (error) {
         // Trata erros na criação da publicação
         return res.status(500).json({ error: "Erro ao criar publicação" });
     }
-
 };
 
+// Função para listar todas as publicações
 export const listAllPublicacao = async (req, res) => {
-
     try {
-
+        // Busca todas as publicações e inclui os dados do usuário relacionado
         const publicacoes = await Publicacao.findAll({
             include: [
                 {
                     model: Usuario,
-                    attributes: ['nick', 'imagem']
+                    attributes: [
+                        'nick', 
+                        'imagem'
+                    ], // Seleciona apenas os atributos nick e imagem do usuário
                 },
-            ]
+            ],
         });
 
+        // Formata os dados das publicações para o retorno
         const data = publicacoes.map(publicacao => ({
             id: publicacao.id,
             publicacao: publicacao.publicacao,
             usuario_id: publicacao.usuario_id,
-            nick: publicacao.nick,
-            imagem: publicacao.imagem,
-            qtd_likes: publicacao.qtd_likes,
-            criado_em: publicacao.criado_em,
+            nick: publicacao.nick, // Obtém o nick do usuário relacionado
+            imagem: publicacao.imagem, // Obtém a imagem do usuário relacionado
+            qtd_likes: publicacao.qtd_likes, // Quantidade de likes na publicação
+            criado_em: new Date(), // Data de criação da publicação
         }));
 
         // Retorna as publicações e o total
         return res.status(200).json({
-            data,
-            total: publicacoes.length,
+            data, // Dados das publicações
+            total: publicacoes.length, // Total de publicações encontradas
         });
-
     } catch (error) {
         // Trata erros na listagem das publicações
         return res.status(500).json({ error: "Erro ao buscar publicações" });
     }
 };
 
+// Função para listar todas as publicações de um usuário específico
 export const listAllUsuario = async (req, res) => {
     try {
-        const usuario_id = req.params.usuario_id;
+        const usuario_id = req.params.usuario_id; // Obtém o ID do usuário dos parâmetros da URL
 
         // Busca o usuário pelo ID
         const usuario = await Usuario.findByPk(usuario_id);
@@ -87,15 +89,18 @@ export const listAllUsuario = async (req, res) => {
 
         // Busca publicações do usuário com os relacionamentos necessários
         const publicacoes = await Publicacao.findAll({
-            where: { usuario_id },
+            where: { usuario_id }, // Filtra publicações pelo ID do usuário
             include: [
                 {
                     model: Usuario,
-                    attributes: ['nick', 'imagem']
+                    attributes: [
+                        'nick', 
+                        'imagem'
+                    ], // Seleciona atributos do usuário relacionado
                 },
                 {
                     model: Comentario,
-                    as: 'Comments',
+                    as: 'Comments', // Inclui os comentários relacionados
                 },
             ],
         });
@@ -105,92 +110,101 @@ export const listAllUsuario = async (req, res) => {
             publicacao_id: publicacao.id,
             publicacao: publicacao.publicacao,
             usuario_id: publicacao.usuario_id,
-            nick: publicacao.Usuario.nick,
-            imagem: publicacao.Usuario.imagem,
-            qtd_likes: publicacao.qtd_likes,
-            qtd_comentarios: publicacao.Comments ? publicacao.Comments.length : 0,
-            criado_em: new Date(),
+            nick: publicacao.Usuario.nick, // Obtém o nick do usuário relacionado
+            imagem: publicacao.Usuario.imagem, // Obtém a imagem do usuário relacionado
+            qtd_likes: publicacao.qtd_likes, // Quantidade de likes na publicação
+            qtd_comentarios: publicacao.Comments ? publicacao.Comments.length : 0, // Quantidade de comentários
+            criado_em: new Date(), // Data atual como data de criação
         }));
 
         // Retorna as publicações e o total
         return res.status(200).json({
-            data,
-            total: publicacoes.length
+            data, // Dados das publicações
+            total: publicacoes.length, // Total de publicações encontradas
         });
     } catch (error) {
         return res.status(500).json({ erro: "Erro interno do servidor" });
     }
 };
 
+// Função para buscar uma única publicação
 export const listOnePublicacao = async (req, res) => {
-
     try {
-        const publicacao_id = req.params.publicacao_id;
-
-        const publicacaoE = await Publicacao.findByPk(publicacao_id);
-        
-        // Verifica se a publicação existe
-        if (!publicacaoE) {
-            return res.status(404).json({ erro: "Publicação não encontrada" });
-        }
+        const publicacao_id = req.params.publicacao_id; // Obtém o ID da publicação dos parâmetros da URL
 
         // Busca a publicação específica e inclui dados do usuário e comentários
         const publicacao = await Publicacao.findOne({
-            where: { publicacao_id },
+            where: { publicacao_id }, // Filtra pela publicação com o ID informado
             attributes: [
-                'publicacao_id',
-                'publicacao',
-                'usuario_id',
-                'qtd_likes',
-                'criado_em'
-            ],
+                'id', 
+                'publicacao', 
+                'usuario_id', 
+                'qtd_likes'
+            ], // Seleciona os atributos necessários da publicação
             include: [
                 {
                     model: Usuario,
-                    attributes: ['nick', 'imagem']
-                }
-            //     {
-            //         model: Comentario,
-            //         //as: 'Comments',
-            //         attributes: [
-            //             'comentario_id',
-            //             'comentario',
-            //             'usuario_id',
-            //             'qtd_likes',
-            //             'criado_em'
-            //         ],
-            //     }
-            // ],
-            ]
+                    attributes:[
+                        'nick', 
+                        'imagem'
+                    ], // Inclui dados do usuário relacionado
+                },
+                {
+                    model: Comentario,
+                    attributes: [
+                        'id',
+                        'comentario', 
+                        'usuario_id', 
+                        'qtd_likes'
+                    ], // Inclui os atributos necessários dos comentários
+                    include: {
+                        model: Usuario,
+                        attributes: [
+                            'nick', 
+                            'imagem'
+                        ], // Inclui dados do usuário autor do comentário
+                    },
+                },
+            ],
         });
-        
-        const data = publicacoes.map(publicacao => ({
-            publicacao_id: publicacao.id,
-            publicacao: publicacao.publicacao,
-            usuario_id: publicacao.usuario_id,
-            nick: publicacao.Usuario.nick,
-            imagem: publicacao.Usuario.imagem,
-            qtd_likes: publicacao.qtd_likes,
-            criado_em: new Date(),
-        }));
+
+        // Verifica se a publicação existe
+        if (!publicacao) {
+            return res.status(404).json({ erro: "Publicação não encontrada" });
+        }
 
         // Retorna a publicação e seus detalhes
-        return res.status(200).json({
-            data    
-        });
-
-    } catch (erro) {}
-
+        return res.status(200).json(
+            publicacao.map(publicacao => ({
+                publicacao: publicacao.publicacao,
+                usuario_id: publicacao.usuario_id,
+                nick: publicacao.nick, // Nick do autor da publicação
+                imagem: publicacao.imagem, // Imagem do autor da publicação
+                qtd_likes: publicacao.qtd_likes, // Quantidade de likes na publicação
+                criado_em: new Date(), // Data atual
+                comentarios: publicacao.Comentarios.map(comentario => ({
+                    comentario_id: comentario.id,
+                    comentario: comentario.comentario,
+                    usuario_id: comentario.usuario_id,
+                    nick: comentario.nick, // Nick do autor do comentário
+                    imagem: comentario.imagem, // Imagem do autor do comentário
+                    qtd_likes: comentario.qtd_likes, // Quantidade de likes no comentário
+                    criado_em: new Date(), // Data atual
+                })),
+            }))
+        );
+    } catch (error) {
+        return res.status(500).json({ erro: "Erro interno do servidor" });
+    }
 };
 
+// Função para deletar uma publicação
 export const deletePublicacao = async (req, res) => {
-
-
     try {
-        const { publicacao_id, usuario_id } = req.body;
-    
-        const publicacao = await Publicacao.findByPk(publicacao_id);
-    
+        const { publicacao_id, usuario_id } = req.body; // Obtém os IDs da requisição
+
+        const publicacao = await Publicacao.findByPk(publicacao_id); // Busca a publicação pelo ID
+
         // Verifica se a publicação existe
         if (!publicacao) {
             return res.status(400).json({ erro: "Publicação não encontrada" });
@@ -211,7 +225,7 @@ export const deletePublicacao = async (req, res) => {
 
         // Retorna uma mensagem de sucesso
         return res.status(200).json({ mensagem: "Publicação deletada com sucesso" });
-
-    } catch (error) {}
-
+    } catch (error) {
+        return res.status(500).json({ erro: "Erro interno do servidor" });
+    }
 };
